ba2399b5a212e251f60f023bd0a625818bf0ef42
Davin <davinsolano@gmail.com>
2019-02-27 17:48:53 +0800

    Added Bam

--- loginscrolling.js
+++ loginscrolling.js
@@ -123,7 +123,7 @@ function insertToRedis(list,onRep,message){
         //25 pages limit i set, scroll down until 25 pages
         console.log(list.length);
         console.log("at "+ra);
-        if(list.length >= 29 && ra <= 165){
+        if(list.length >= 29 && ra <= 175){
             ra++;
             scrollDown();
         }
--- trendcrawl.js
+++ trendcrawl.js
@@ -9,7 +9,8 @@ const extra= require("./extra");
 const NTtop_API = "https://static.social-api.me/exports/buffer/json/instant-game-nt-multi-startpage-top-language-json-english.json";
 const NTmid_API = "https://static.social-api.me/exports/buffer/json/instant-game-nt-multi-startpage-middle-language-json-english.json";
 const wowAPI = "https://api.wowwquiz.com/v2/quiz_recommend_list.php?page=0&size=20&lang=en";
-const finishUP = [1,1,0];
+const bamAPI = "https://www.bamapp.info/api/quizzes/list?gender=none&locale=en";
+const finishUP = [1,1,1,0];
 
 
 async function trendcrawl(){
@@ -58,7 +59,19 @@ async function trendcrawl(){
                shipment = [];
                // console.log(something);
                collectDataWow(something).then(function(res){
-                   endTheCode(2,browser);
+                   trendBAM(page);
+               });
+
+           });
+        }
+
+        else if (response.url().indexOf("https://www.bamapp.info/api/quizzes/") != -1) {
+           var bamList = response.json().then(function(something){
+
+               shipment = [];
+               // console.log(something);
+               collectDataBam(something).then(function(res){
+                   endTheCode(3,browser);
                });
 
            });
@@ -68,6 +81,7 @@ async function trendcrawl(){
 
 
     trendNTtop(page);
+    // trendBAM(page);
     // trendNTmid(page1);
     // trendWOW(page2);
 
@@ -91,6 +105,10 @@ async function trendNTmid(page1){
 }
 async function trendWOW(page2){
     await page2.goto(wowAPI);
+}
+
+async function trendBAM(page2){
+    await page2.goto(bamAPI);
 }
 
 async function collectDataNTtop(something){
@@ -271,8 +289,107 @@ async function collectDataWow(something){
                     return res1;
                 });
             }
+
         });
     });
+
+    // console.log(listInString);
+}
+
+async function collectDataBam(something){
+    var aDate = new Date();
+    aDate = new Date(aDate.getUTCFullYear(),aDate.getUTCMonth(),aDate.getUTCDate(),aDate.getUTCHours());
+    var nowDate = dateTranslate(aDate);
+
+    var on_temp = [];
+
+    var listInString="";
+
+    var x = 0;
+    var xx = 0;
+
+    console.log(something.male.length);
+    console.log(something.female.length);
+    console.log(something.none.length);
+    console.log("|||");
+    for(let x = 0; x < something.none.length; x++){
+        listInString = x == 0 ? listInString + something.none[x].id : listInString + "|||" + something.none[x].id;
+        let a = createARecordBam(something.none[x],nowDate,aDate);
+        on_temp.push(a);
+    }
+
+    console.log(listInString.split("|||").length);
+
+    for(let x = 0; x < something.male.length; x++){
+        if(listInString.indexOf(something.male[x].id) == -1){
+            console.log("Unique");
+            listInString = listInString.length <= 0 ? listInString + something.male[x].id : listInString + "|||" + something.male[x].id;
+            let a = createARecordBam(something.male[x],nowDate,aDate);
+            on_temp.push(a);
+        }
+    }
+
+    console.log(listInString.split("|||").length);
+
+    for(let x = 0; x < something.female.length; x++){
+        if(listInString.indexOf(something.female[x].id) == -1){
+            console.log("Special");
+            listInString = listInString.length <= 0 ? listInString + something.female[x].id : listInString + "|||" + something.female[x].id;
+            let a = createARecordBam(something.female[x],nowDate,aDate);
+            on_temp.push(a);
+        }
+    }
+
+    console.log(listInString.split("|||").length);
+
+    return Promise.all(on_temp).then(function(val){
+        var trendHash = "trend >>> "+nowDate;
+        var currDate = new Date();
+        var trendKey = "bam >>> "+aDate.getHours()+":"+"00";
+
+        return extra.hexistsAsync(trendHash,trendKey).then(function(res){
+
+            if (res == 1){
+                return extra.hgetAsync(trendHash,trendKey).then(function(res1){
+                    var tempListString = listInString.split("|||");
+                    var tempres1 = res1.split("|||");
+                    // console.log(tempres1.length);
+                    // console.log(tempres1);
+                    for(let iii = 0; iii < tempListString.length; iii++){
+                        if( !tempres1.includes(tempListString[iii]) ){
+                            // console.log(tempres1);
+                            console.log("Not Exist");
+                            console.log(tempListString[iii]);
+                            tempres1.push(tempListString[iii]);
+                        }
+                    }
+                    // console.log(tempres1.length);
+                    // console.log(tempres1);
+                    tempres1 = tempres1.join('|||');
+
+                    return extra.hsetAsync(trendHash,trendKey,tempres1).then(function(res1){
+                        console.log(trendHash);
+                        console.log(trendKey);
+                        console.log("Update Done Data bam");
+                        return res1;
+                    });
+
+                });
+            }
+            else{
+                return extra.hsetAsync(trendHash,trendKey,listInString).then(function(res1){
+                    console.log(trendHash);
+                    console.log(trendKey);
+                    console.log("Done Data bam");
+                    return res1;
+                });
+            }
+
+        });
+
+    });
+
+    // console.log(Object.keys(something.none[0]));
 
     // console.log(listInString);
 }
@@ -405,8 +522,72 @@ function createARecordWow(payload,nowDate,aDate){
                 else{
                     return 1;
                 }
+
             });
         }
+    });
+}
+
+function createARecordBam(payload,nowDate,aDate){
+    var tempKey = "bam >>> "+payload.id;
+    var last_seen = nowDate+"-"+aDate.getHours()+":00";
+
+    return extra.existsAsync(tempKey).then(function(res){
+        var isKey = "reco_appearances";
+        var isKeyDate = "last_seen";
+        if(payload.thumb == null){
+            payload.thumb = "null";
+        }
+        if(payload.il_thumb == null){
+            payload.il_thumb = "null";
+        }
+        if(payload.featured_thumb == null){
+            payload.featured_thumb = "null";
+        }
+
+        if(res == 0){
+            console.log(tempKey);
+            return extra.hmsetAsync(tempKey,
+                        "id",payload.id,
+                        "title",payload.title,
+                        "thumb",payload.thumb,
+                        "featured_thumb",payload.featured_thumb,
+                        "il_thumb",payload.il_thumb,
+                        "last_seen",last_seen,
+                        "reco_appearances",1).then(function(res){
+                            return extra.saddAsync("archive_id_bam",payload.id).then(function(res1){
+                                return res;
+                            });
+                        });
+        }
+
+        else{
+            return extra.hgetallAsync(tempKey).then(function(res1){
+
+                var isCurr = res1.reco_appearances;
+                var isCurrDate = res1.last_seen.split("-");
+                isCurrDate[3] = isCurrDate[3].split(':');
+                isCurrDate = new Date(parseInt(isCurrDate[0]),parseInt(isCurrDate[1])-1,parseInt(isCurrDate[2]),parseInt(isCurrDate[3][0]));
+                var temp_last_seen = last_seen.split("-");
+                temp_last_seen[3] = temp_last_seen[3].split(':');
+                temp_last_seen = new Date(parseInt(temp_last_seen[0]),parseInt(temp_last_seen[1])-1,parseInt(temp_last_seen[2]),parseInt(temp_last_seen[3][0]));
+                verdict = temp_last_seen > isCurrDate ? true:false;
+
+                if(verdict == true){
+                    // console.log("Success");
+                    // console.log(tempKey);
+                    var isAmount = parseInt(isCurr) + 1;
+                    return extra.hmsetAsync(tempKey,isKey,isAmount,isKeyDate,last_seen).then(function(res2){
+                            return res2;
+                    })
+                }
+                else{
+                    return 1;
+                }
+
+            });
+        }
+
     });
 }
 
